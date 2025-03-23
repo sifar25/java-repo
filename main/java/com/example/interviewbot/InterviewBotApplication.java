f6bd09024a15516ae978b8df5efa20261946db8c
Sign Teched <sign.teched@gmail.com>
2025-03-24 04:45:12 +0530

    my commit

---
+++ main/java/com/example/interviewbot/InterviewBotApplication.java
@@ -0,0 +1,12 @@
+package com.example.interviewbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewBotApplication.class, args);
+    }
+} 
